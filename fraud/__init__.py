f84dd85fd83e4b26b5ad803a2085ffaec6e32dc4
Jessie-ZhaoXi <77218431+Jessie-ZhaoXi@users.noreply.github.com>
2024-04-10 19:00:22 +0800

    feat: modularise url parsing functions

--- fraud/__init__.py
+++ fraud/__init__.py
@@ -13,6 +13,11 @@ from .features.pre_processing import (
     select_top_k_based_on_mutual_information
 )
 
+from .features.build_features import(
+    process_new_url,
+    reformat_df
+)
+
 from .models.lazy_predict import (lazy_predict,)
 from .models.predict_model import (predict,)
 from .models.train_model import (train,)
